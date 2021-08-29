486fc7f013e6079f33e3fc32aeded82b11be6af2
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-08-29 01:08:21 -0300

    Add files via upload

---
+++ pages/api/developer.js
@@ -0,0 +1,11 @@
+function main(request, response) {
+    
+    response.json({
+        name: "Pedro Szabo Silva",
+        discord: "yPepê#7252",
+        youtube: "Pepe_Java",
+        MCNick: "Pepe_Java",
+        Twitch: "Pepe_Java"
+    })
+
+}
