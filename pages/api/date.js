2162c46a20222db987b19db737232da338705839
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-09-10 21:43:44 -0300

    Update date.js

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,10 +1,28 @@
 function main(request, response) {
+
     const dynamicDate = new Date();
-    
+
     response.json({
-        date: dynamicDate.toGMTString()
-    })
-    
+        Date: dynamicDate.getDate(),
+        Day: dynamicDate.getDay(),
+        FullYear: dynamicDate.getFullYear(),
+        Hours: dynamicDate.getHours(),
+        Milliseconds: dynamicDate.getMilliseconds(),
+        Minutes: dynamicDate.getMinutes(),
+        Month: dynamicDate.getMonth(),
+        Secounds: dynamicDate.getSeconds(),
+        Time: dynamicDate.getTime(),
+        TimezoneOffset: dynamicDate.getTimezoneOffset(),
+        UTCDate: dynamicDate.getUTCDate(),
+        UTCDay: dynamicDate.getUTCDay(),
+        UTCFullYear: dynamicDate.getUTCFullYear(),
+        UTCHours: dynamicDate.getUTCHours(),
+        UTCMilliseconds: dynamicDate.getUTCMilliseconds(),
+        UTCMinutes: dynamicDate.getUTCMinutes(),
+        UTCMonth: dynamicDate.getUTCMonth(),
+        UTCSeconds: dynamicDate.getUTCSeconds()
+    })    
+
 }
 
 export default main;
